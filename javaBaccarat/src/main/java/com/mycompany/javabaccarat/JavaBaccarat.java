fd91ab1b337130a68a3c0f1ecb736bbb218bcd4e
Lance <lancebribrav@gmail.com>
2025-04-28 14:07:46 +0800

    Add files via upload

---
+++ javaBaccarat/src/main/java/com/mycompany/javabaccarat/JavaBaccarat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javabaccarat;
+
+/**
+ *
+ * @author Lance
+ */
+public class JavaBaccarat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
